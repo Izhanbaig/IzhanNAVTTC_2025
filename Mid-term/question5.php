216055544cc4f799f13cf535d1d045ad466a0f66
Izhanbaig <thisisizhan4@gmail.com>
2025-04-24 17:51:39 +0500

    Add files via upload

---
+++ Mid-term/question5.php
@@ -0,0 +1,25 @@
+<?php
+$num1=15;
+$num2=6;
+$operator="*";
+
+
+if ($operator=="+") {
+
+    $result = $num1 + $num2 ;
+
+} else if ($operator=="-") {
+
+  $result = $num1 - $num2;
+
+} else if ($operator=="*") {
+
+  $result = $num1 * $num2;
+
+}
+  
+
+
+
+ echo "$result is the answer";
+?>
